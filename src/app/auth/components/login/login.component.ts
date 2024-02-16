cdfa5eadc6d88e3bb3994d4e924384c8b5988ec4
Seif Tlijani <114825087+tlijani-ux@users.noreply.github.com>
2024-02-16 23:09:42 +0100

    add Service Storage

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth/auth.service';
-
+import { StorageService } from '../../services/storage/storage.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -15,7 +16,8 @@ export class LoginComponent {
 
 
   constructor(private fb: FormBuilder,
-    private authService: AuthService
+    private authService: AuthService,
+    private message: NzMessageService
   ) { }
 
   ngOnInit() {
@@ -31,9 +33,24 @@ export class LoginComponent {
     console.log(this.loginform?.value);
     this.authService.login(this.loginform.value).subscribe((res) =>{
       console.log(res);
-   
-    })
-  }
+      //let user = null; 
+
+      if (res.userId!=null){
+        const user = { 
+          id: res.userId,
+          role: res.userRole
+        }
+      StorageService.saveToken(res.jwt);
+      StorageService.setUser(user);
+
+      }else{
+        this.message.error("Bad credentials" , {nzDuration:5000});
+        
+
+      }
+
+    });
+}
 
 
 }
--- src/app/auth/services/storage/storage.service.ts
+++ src/app/auth/services/storage/storage.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
 
+
+const USER_KEY = 'auth-user';
+const TOKEN_KEY = 'auth-token';
+
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
 
   constructor() { }
+
+
+  static saveToken(token: string): void {
+    window.sessionStorage.removeItem(TOKEN_KEY);
+    window.sessionStorage.setItem(TOKEN_KEY, token);
+  }
+
+  static setUser(user: any): void {
+    window.sessionStorage.removeItem(USER_KEY);
+    window.sessionStorage.setItem(USER_KEY, JSON.stringify(user));
+  }
+
+
+  static getToken() {
+    return window.localStorage.getItem(TOKEN_KEY);
+  }
+
+  static getUser(): any {
+    const user = window.sessionStorage.getItem(USER_KEY);
+    if (user) {
+      return JSON.parse(user);
+    }
+    return {};
+  }
+
+
+  static getUserRole(): string {
+    const user = this.getUser();
+    return user.role;
+  }
+
+  static isAdminLoggedIn(): boolean {
+
+    if (this.getToken == null) return false;
+    const role: string = this.getUserRole();
+    return role == 'ADMIN';
+
+  }
+
+  static isCustomerLoggedIn(): boolean {
+    if (this.getToken == null) return false;
+    const role: string = this.getUserRole();
+    return role == 'CUSTOMER';
+  }
 }
